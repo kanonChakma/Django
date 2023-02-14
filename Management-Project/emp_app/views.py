50977016bc87d7ba4ce2073b32d64610afb84b6f
kanon chakma <kanonchakma@pop-os.localdomain>
2023-02-14 13:42:01 +0600

    feat: added employment functionality

--- Management-Project/emp_app/views.py
+++ Management-Project/emp_app/views.py
@@ -1,19 +1,48 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Department, Employee, Role
+
 
 # Create your views here.
 def index(request):
     # return HttpResponse("hello")
     return render(request, 'emp_app/index.html')
 
+
 def add_emp(request):
-    return render(request, 'emp_app/add_emp.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        dept = int(request.POST['dept'])
+        salary = int(request.POST['salary'])
+        bonus = int(request.POST['bonus'])
+        role = int(request.POST['role'])
+        phone = int(request.POST['phone'])
+
+        new_emp = Employee(first_name=first_name, last_name=last_name,dept_id=dept,salary=salary,bonus=bonus,role_id=role,phone=phone)
+        new_emp.save()
+        return HttpResponse("Employee added Successfully")
+
+    elif request.method == 'GET':
+        return render(request, 'emp_app/add_emp.html')
+    else:
+       return HttpResponse("Employee not added") 
+
+    
+
 
 def remove_emp(request):
     return render(request, 'emp_app/remove_emp.html')
 
 def view_emp(request):
-    return render(request, 'emp_app/view_emp.html')
+    emps = Employee.objects.all()
+    context = {
+        'emps': emps
+    }
+    print(context)
+    return render(request, 'emp_app/view_emp.html',context)
 
 def filter_emp(request):
     return render(request, 'emp_app/filter_emp.html')
+ 
