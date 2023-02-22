76ce209e2fc50e34ecb42f30d42f232d892fbfea
kanon chakma <kanonchakma@pop-os.localdomain>
2023-02-22 19:36:14 +0600

    added templates

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+posts =[
+       {
+           "name": "post1" 
+        },
+        {
+            "name": "post2"
+        }
+    
+]
+
+def home(request):
+    context={
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
